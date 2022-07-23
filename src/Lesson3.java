82a9cd92a1453dfef848bc99885640bffa060760
HomePC <tarikcollu@gmail.com>
2022-07-23 02:04:00 -0400

    This is third update

---
+++ src/Lesson3.java
@@ -0,0 +1,11 @@
+public class Lesson3 {
+
+    public static void main(String[] args) {
+
+        String name = "Tarik";
+        System.out.println(name);
+
+        int model = 2022;
+        System.out.println(model);
+    }
+}
