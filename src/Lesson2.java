9e4d4453185a4063122810e01681ed554e7ab99f
HomePC <tarikcollu@gmail.com>
2022-07-23 01:59:38 -0400

    This is update

---
+++ src/Lesson2.java
@@ -0,0 +1,13 @@
+public class Lesson2 {
+
+    public static void main(String[] args) {
+
+        int age = 5;
+        int age2 = 6;
+
+
+        System.out.println(age);
+        System.out.println(age2);
+        System.out.println(age + age2);
+    }
+}
